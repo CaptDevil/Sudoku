8d7af36662003f2d8b84455fadc368dff028454a
CaptDevil <prabhudutta.sarangi@gmail.com>
2020-05-20 23:35:59 +0530

    changes in side.js file

--- script/side.js
+++ script/side.js
@@ -7,13 +7,17 @@ let today = new Date();
 
 let hours = today.getHours();
 
-if(hours>=0 && hours<6)
-    timeStamp.innerHTML = `6:00 AM IST ${today.getDate()}-${(today.getMonth()+1)}-${today.getFullYear()}`;
-else if(hours>=6 && hours<12)
+if(hours>=0 && hours<4)
+    timeStamp.innerHTML = `4:00 AM IST ${today.getDate()}-${(today.getMonth()+1)}-${today.getFullYear()}`;
+else if(hours>=4 && hours<8)
+    timeStamp.innerHTML = `8:00 AM IST ${today.getDate()}-${(today.getMonth()+1)}-${today.getFullYear()}`;
+else if(hours>=8 && hours<12)
     timeStamp.innerHTML = `12:00 PM IST ${today.getDate()}-${(today.getMonth()+1)}-${today.getFullYear()}`;
-else if(hours>=12 && hours<18)
-    timeStamp.innerHTML = `6:00 PM IST ${today.getDate()}-${(today.getMonth()+1)}-${today.getFullYear()}`;
-else if(hours>=18 && hours<24)
+else if(hours>=12 && hours<16)
+    timeStamp.innerHTML = `4:00 PM IST ${today.getDate()}-${(today.getMonth()+1)}-${today.getFullYear()}`;
+else if(hours>=16 && hours<20)
+    timeStamp.innerHTML = `8:00 PM IST ${today.getDate()+1}-${(today.getMonth()+1)}-${today.getFullYear()}`;
+else if(hours>=20 && hours<24)
     timeStamp.innerHTML = `00:00 AM IST ${(today.getDate()+1)}-${(today.getMonth()+1)}-${today.getFullYear()}`;
 
 let countdown = setInterval(() => {
